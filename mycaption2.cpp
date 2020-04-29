64792bded15bb12d93f0b9f796cfb7323a7e3640
rahul777-oss <63468051+rahul777-oss@users.noreply.github.com>
2020-04-29 18:55:31 +0530

    Add files via upload

---
+++ mycaption2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ 
+ int main ()
+ {
+ 	int age;
+ 	cout<<"enter your age: "<<endl;
+ 	cin>>age;
+ 	if(age>18)
+ 	{ 
+ 	cout<<"the person can vote"<<endl;
+ 	
+	 }
+	 else
+	 {
+	 
+	 cout<<"the person cannot vote"<<endl;
+}
+	 return 0;
+	  }
